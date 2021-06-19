e7dffd3149cdc02d9c1eefc6c6dcb1cab80add43
kfield1 <kayt1303@yahoo.com>
2021-06-19 11:11:44 -0500

    app more

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -9,5 +9,12 @@ d3.json('samples.json').then(function(theData)) {
         x: labels,
         y:values
     }];
-    
+
+    var layout = {
+        margin: { 1: 0, b:0, t:0}
+        width:450,
+        height:450
+    };
+
+    Plotly.newPlot('something', data, layout);
 }
